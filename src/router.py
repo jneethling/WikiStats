37dce40829e43828d5472ccba64014ac2cc77712
Hannes Neethling <hannes@Hanness-MacBook-Pro.local>
2021-11-30 10:48:38 +0000

    sorted project structure

---
+++ src/router.py
@@ -0,0 +1,92 @@
+import os
+import psutil
+import tornado.web
+import tornado.ioloop
+import concurrent.futures
+
+# Custom class
+from .handlers import CustomHandler
+
+executor = concurrent.futures.ThreadPoolExecutor(max_workers=4,)
+
+class pingHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        
+        self.write('pong')
+        self.finish()
+
+class memoryHandler(tornado.web.RequestHandler):
+
+    def get(self):
+        memory = 1024 * 1024
+        proc = psutil.Process(os.getpid())
+        mem0 = proc.memory_info().rss
+        msg = str(mem0/memory) + 'Mb'
+        response = {'Memory use':msg}
+        self.write(response)
+        self.finish()
+
+class statusHandler(tornado.web.RequestHandler):
+
+    def initialize(self, ref_obj):       
+        self.ref_obj = ref_obj
+
+    async def get(self):
+        res = await tornado.ioloop.IOLoop.current().run_in_executor(executor, self.ref_obj.getStatus)
+        self.write(res)
+
+class totalsHandler(tornado.web.RequestHandler):
+
+    def initialize(self, ref_obj):        
+        self.ref_obj = ref_obj
+
+    async def get(self):
+        res = await tornado.ioloop.IOLoop.current().run_in_executor(executor, self.ref_obj.getTotals)
+        self.write(res)
+
+class countsHandler(tornado.web.RequestHandler):
+
+    def initialize(self, ref_obj):        
+        self.ref_obj = ref_obj
+
+    async def get(self):
+        res = await tornado.ioloop.IOLoop.current().run_in_executor(executor, self.ref_obj.getCounts)
+        self.write(res)
+
+class startHandler(tornado.web.RequestHandler):
+
+    def initialize(self, ref_obj):       
+        self.ref_obj = ref_obj
+
+    async def get(self):
+        res = await tornado.ioloop.IOLoop.current().run_in_executor(executor, self.ref_obj.startWork)
+        self.write(res)
+
+class stopHandler(tornado.web.RequestHandler):
+
+    def initialize(self, ref_obj):        
+        self.ref_obj = ref_obj
+
+    async def get(self):
+        res = await tornado.ioloop.IOLoop.current().run_in_executor(executor, self.ref_obj.stopWork)
+        self.write(res)
+
+def make_app(c_handler):
+    return tornado.web.Application([
+                    (r"/ping", pingHandler),
+                    (r"/memory", memoryHandler),
+                    (r"/status", statusHandler, {"ref_obj":c_handler}),
+                    (r"/start", startHandler, {"ref_obj":c_handler}),
+                    (r"/stop", stopHandler, {"ref_obj":c_handler}),
+                    (r"/totals", totalsHandler, {"ref_obj":c_handler}),
+                    (r"/counts", countsHandler, {"ref_obj":c_handler})
+                    ])
+
+if __name__ == '__main__':
+
+    c_handler = CustomHandler()
+    application = make_app(c_handler)
+    application.listen(8881)
+    print('Server started on port 8881')
+    tornado.ioloop.IOLoop.instance().start()
---
+++ tests/test_endpoints.py
@@ -0,0 +1,44 @@
+from tornado.testing import AsyncHTTPTestCase
+import json
+from src.handlers import CustomHandler
+from src import router
+
+class TestHandlers(AsyncHTTPTestCase):
+    def get_app(self):
+        c_handler = CustomHandler()
+        return router.make_app(c_handler)
+
+    def test_getPing(self):
+        response = self.fetch("/ping")
+        self.assertEqual(response.code, 200)
+        self.assertEqual(response.body, b'pong')
+
+    def test_getMemory(self):
+        response = self.fetch("/memory")
+        self.assertEqual(response.code, 200)
+        self.assertIn("Memory use", json.loads(response.body))
+
+    def test_getStatus(self):
+        response = self.fetch("/status")
+        self.assertEqual(response.code, 200)
+        resp = json.loads(response.body)
+        self.assertIn("Status", resp)
+        self.assertIn("Message", resp)
+        self.assertIn("Working in background", resp)
+        self.assertIn("Records in session", resp)
+
+    def test_getTotals(self):
+        response = self.fetch("/totals")
+        self.assertEqual(response.code, 200)
+        try:
+            json.loads(response.body)
+        except ValueError:
+            self.fail("Response is not valid json!")
+
+    def test_getCount(self):
+        response = self.fetch("/counts")
+        self.assertEqual(response.code, 200)
+        try:
+            json.loads(response.body)
+        except ValueError:
+            self.fail("Response is not valid json!")
